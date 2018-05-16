4fe555d93c8fec22cc4e819de7dec960d327198f
Ardhendu Singh <ardsingh@cisco.com>
2018-05-16 08:20:29 -0400

    For Heroku changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -61,6 +62,6 @@ app.get('/bad', (req, res) => {
 
 // port to listen
 // go to safari http://localhost:3000
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
